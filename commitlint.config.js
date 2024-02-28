fae8e05d1ef2abca80a29a8757601947977fb2f8
yyh <2418207853@qq.com>
2024-02-28 15:18:03 +0800

    ci: implement commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,34 @@
+// build: 影响构建系统或外部依赖项的更改（示例范围：gulp、broccoli、npm）
+// ci: 更改我们的 CI 配置文件和脚本（示例范围：Travis、Circle、BrowserStack、SauceLabs）
+// docs: 文档修改
+// feat: 一个新的功能
+// fix: 一个 bug 修复
+// perf: 提升性能的代码修改
+// refactor: 既不修复错误也不添加功能的代码更改
+// style: 不影响代码含义的更改（空格、格式、缺少分号等）
+// test: 添加缺失的测试或更正现有测试
+
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    //   TODO Add Scope Enum Here
+    // 'scope-enum': [2, 'always', ['yourscope', 'yourscope']],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'update',
+        'feat',
+        'fix',
+        'docs',
+        'chore',
+        'style',
+        'refactor',
+        'ci',
+        'test',
+        'perf',
+        'revert',
+      ],
+    ],
+  },
+};
