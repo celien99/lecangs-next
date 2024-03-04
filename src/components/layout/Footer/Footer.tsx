e89c79b74913c612de3958a2ea47a953a42d41a2
yyh <2418207853@qq.com>
2024-03-04 11:11:36 +0800

    feat: 底部移动端样式

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -38,6 +38,7 @@ export default function Footer() {
                 alt=''
                 fill
                 sizes='(max-width: 750px) 0, 130px'
+                style={{ objectFit: 'cover' }}
               />
             </div>
             <div className='contact'>
@@ -69,6 +70,59 @@ export default function Footer() {
                   <button type='submit'>
                     <div className='svg'>
                       <Image src='/foot.svg' alt='' fill />
+                    </div>
+                  </button>
+                </div>
+              </div>
+              <p className='hint'>提交邮箱以订阅我们最新的优惠信息和行业文章</p>
+            </div>
+          </div>
+        </div>
+        <div className='content-mobile'>
+          <div className='company-info'>
+            <div className='logo'>
+              <Image
+                src='/logo_amazonspn.png'
+                alt=''
+                fill
+                style={{ objectFit: 'cover' }}
+              />
+            </div>
+            <p>股票简称：乐歌股份</p>
+            <p>股票代码：300729</p>
+          </div>
+          <div className='friend-link'>
+            <p>友情链接</p>
+            <ul className='linkList'>
+              {linkList.map((item, index) => (
+                <li key={index}>
+                  <a href={item.link}>{item.uname}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className='contact-us'>
+            <p>联系我们</p>
+            <p>宁波市鄞州区首南街道学士路536号 金东大厦15-19层</p>
+            <p>400-835-8800</p>
+            <p>info@lecangs.com</p>
+          </div>
+          <div className='bottom'>
+            <h1>订阅乐歌海外仓电子报</h1>
+            <div className='emainInput'>
+              <div className='inputBox'>
+                <div className='input'>
+                  <input type='text' placeholder='邮箱' />
+                </div>
+                <div className='btn'>
+                  <button type='submit'>
+                    <div className='svg'>
+                      <Image
+                        src='/foot.svg'
+                        alt=''
+                        fill
+                        style={{ objectFit: 'cover' }}
+                      />
                     </div>
                   </button>
                 </div>
