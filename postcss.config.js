4bf10c5ed565fe1d6ee04d2f4200861f52470d52
yyh <2418207853@qq.com>
2024-03-04 10:34:00 +0800

    build: vw适配

--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,12 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    '@tcstory/postcss-px-to-viewport': {
+      viewportWidth: 1920,
+      unitPrecision: 5,
+      viewportUnit: 'vw',
+      minPixelValue: 1,
+      mediaQuery: true,
+    },
   },
 };
