ee57cfbfbafb9579678ce61fc60854e6c5481e6f
2418207853@qq.com <2418207853@qq.com>
2024-03-25 22:39:21 +0800

    feat: 供应链服务

---
+++ src/app/(service)/supplyChain/page.tsx
@@ -0,0 +1,26 @@
+import './supply.css';
+export default function page() {
+  return (
+    <>
+      <div className='supply'>
+        <div className='common'>
+          <h2>供应链服务</h2>
+          <h3>
+            我们提供国内代理采购服务，并与多家银行合作，为您提供优质的国内采购资金支持，并且超长账期，超低利率
+          </h3>
+        </div>
+      </div>
+      <div className='advantage'>
+        <div className='common'>
+          <h2>服务优势</h2>
+        </div>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    </>
+  );
+}
